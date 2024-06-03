ec938aa099a607fcdd1fb87182eb3c1f1ed4bc08
danielhill1982 <mail@dhill.dev>
2024-06-03 19:08:01 +0200

    bla

---
+++ pages/[...slug].vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <ContentDoc />
+    <ContentList path="/" v-slot="{ list }">
+      <div v-for="article in list" :key="article._path">
+        <h2>{{ article.title }}</h2>
+        <p>{{ article.description }}</p>
+      </div>
+    </ContentList>
+    <nav>
+    <ContentNavigation v-slot="{ navigation }">
+      <ul>
+        <li v-for="link of navigation" :key="link._path">
+          <NuxtLink :to="link._path">{{ link.title }}</NuxtLink>
+        </li>
+      </ul>
+    </ContentNavigation>
+  </nav>
+  </main>
+</template>
